3890a7ccb4932672c099d15fc47ea29460ccd392
AMEDDAH Ayoub <ayoub.ameddah@gmail.com>
2019-12-31 15:19:06 +0100

    Ameddah: BD

--- Admin/database/localhost.sql
+++ Admin/database/localhost.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 4.9.3
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le :  mer. 18 déc. 2019 à 05:55
--- Version du serveur :  10.4.10-MariaDB
--- Version de PHP :  7.1.33
+-- Hôte : localhost
+-- Généré le :  mar. 31 déc. 2019 à 15:14
+-- Version du serveur :  10.2.27-MariaDB-cll-lve
+-- Version de PHP :  5.5.38
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,8 +19,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `it_ca_db`
+-- Base de données :  `mundiaitca_db`
 --
+CREATE DATABASE IF NOT EXISTS `mundiaitca_db` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `mundiaitca_db`;
 
 -- --------------------------------------------------------
 
@@ -29,8 +31,26 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `activity` (
-  `id` int(20) NOT NULL
+  `id_activity` int(20) NOT NULL,
+  `name_activity` varchar(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `activity`
+--
+
+INSERT INTO `activity` (`id_activity`, `name_activity`) VALUES
+(1, 'Add-Product'),
+(2, 'Modify-Product'),
+(3, 'Delete-Product'),
+(4, 'Add-Category'),
+(5, 'Modify-Category'),
+(6, 'Delete-Category'),
+(7, 'Add-Account'),
+(8, 'Modify-Account'),
+(9, 'Delete-Account'),
+(10, 'Approve-Order'),
+(11, 'Disapprove-Order');
 
 -- --------------------------------------------------------
 
@@ -39,7 +59,9 @@ CREATE TABLE `activity` (
 --
 
 CREATE TABLE `activitylog` (
-  `id` int(20) NOT NULL
+  `id_activity` int(20) NOT NULL,
+  `id_admin` int(20) NOT NULL,
+  `Date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -53,9 +75,9 @@ CREATE TABLE `admin` (
   `full_name` varchar(20) NOT NULL,
   `email` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
-  `role` varchar(20) NOT NULL,
-  `statut` varchar(100) NOT NULL,
-  `mobile` int(100) NOT NULL
+  `role` varchar(1) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `mobile` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -68,7 +90,7 @@ CREATE TABLE `category` (
   `id_cat` int(11) NOT NULL,
   `label_cat` varchar(20) NOT NULL,
   `desc_cat` varchar(100) NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT 0
+  `status` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -76,10 +98,10 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id_cat`, `label_cat`, `desc_cat`, `status`) VALUES
-(1, 'Computers & Laptops', 'bbbbbbblllllll', 0),
-(2, 'kitchen', 'ffffffffff', 0),
-(3, 'test', 'WWWWW', 0),
-(5, 'omaima', 'intissar is a ood person', 1);
+(1, 'Category1', 'Description of category1 google lictionnaire', 0),
+(2, 'category', 'description hhh', 1),
+(3, 'Category', 'descr', 0),
+(4, 'Pc', 'hzefzefjezf', 0);
 
 -- --------------------------------------------------------
 
@@ -103,11 +125,26 @@ CREATE TABLE `costumer` (
 CREATE TABLE `order` (
   `id_order` int(20) NOT NULL,
   `code` varchar(100) NOT NULL,
-  `date` date NOT NULL,
+  `date` datetime NOT NULL,
   `id_cost` int(100) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `id_ville` int(4) NOT NULL,
+  `phone` varchar(10) NOT NULL,
   `total_price` decimal(11,2) NOT NULL,
-  `status` varchar(100) NOT NULL
+  `status` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `order_product`
+--
+
+CREATE TABLE `order_product` (
+  `id_order` int(20) NOT NULL,
+  `id_cost` int(20) NOT NULL,
+  `quantity` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -123,18 +160,22 @@ CREATE TABLE `product` (
   `weight_prod` decimal(11,2) NOT NULL,
   `desc_prod` varchar(100) NOT NULL,
   `image_prod` varchar(100) NOT NULL,
-  `available_stock` tinyint(1) NOT NULL,
-  `hide_show` tinyint(1) NOT NULL,
-  `quantity_prod` int(20) NOT NULL
+  `active` tinyint(1) DEFAULT 0,
+  `quantity_prod` decimal(11,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `product`
 --
 
-INSERT INTO `product` (`id_prod`, `label_prod`, `id_cat`, `price_prod`, `weight_prod`, `desc_prod`, `image_prod`, `available_stock`, `hide_show`, `quantity_prod`) VALUES
-(1, 'Whirepool Refrigerator 1.20X0.70', 3, '3000.00', '20.90', 'bllllllllaaaaa description', 'img is here', 0, 0, 10),
-(5, 'pc i5 4 genr', 5, '2000.00', '23.30', 'tdesription 2', 'balanla', 1, 1, 2);
+INSERT INTO `product` (`id_prod`, `label_prod`, `id_cat`, `price_prod`, `weight_prod`, `desc_prod`, `image_prod`, `active`, `quantity_prod`) VALUES
+(1, 'Whirepool Refrigerator 1.20X0.70 9.99', 0, '3000.00', '20.90', 'bllllllllaaaaa description', 'img is here', 0, '10.00'),
+(5, 'pc i5 ', 0, '2000.00', '23.30', 'tdesription 2', 'balanla', 1, '2.00'),
+(6, '', 0, '0.00', '0.00', '', '', 0, '0.00'),
+(7, 'Asus', 0, '5000.00', '6.00', 'PC Portable', 'Koala.jpg', 0, '5.00'),
+(8, 'PC HG', 0, '6666.00', '999.00', 'ffdgdgd', 'Penguins.jpg', 0, '4464.00'),
+(9, 'P1', 0, '2.00', '2.00', 'DESC', 'forest-3840x2160-4k-5k-wallpaper-trees-sunlight-fog-autumn-5726.jpg', 0, '2.00'),
+(10, 'P', 3, '2.00', '2.00', 'DESC', 'forest-3840x2160-4k-5k-wallpaper-trees-sunlight-fog-autumn-5726.jpg', 0, '2.00');
 
 --
 -- Index pour les tables déchargées
@@ -144,13 +185,13 @@ INSERT INTO `product` (`id_prod`, `label_prod`, `id_cat`, `price_prod`, `weight_
 -- Index pour la table `activity`
 --
 ALTER TABLE `activity`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_activity`);
 
 --
 -- Index pour la table `activitylog`
 --
 ALTER TABLE `activitylog`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_activity`,`id_admin`,`Date`);
 
 --
 -- Index pour la table `admin`
@@ -177,6 +218,12 @@ ALTER TABLE `order`
   ADD PRIMARY KEY (`id_order`);
 
 --
+-- Index pour la table `order_product`
+--
+ALTER TABLE `order_product`
+  ADD PRIMARY KEY (`id_order`,`id_cost`);
+
+--
 -- Index pour la table `product`
 --
 ALTER TABLE `product`
@@ -190,13 +237,7 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT pour la table `activity`
 --
 ALTER TABLE `activity`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `activitylog`
---
-ALTER TABLE `activitylog`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_activity` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT pour la table `admin`
@@ -208,7 +249,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT pour la table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id_cat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_cat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `costumer`
@@ -226,7 +267,7 @@ ALTER TABLE `order`
 -- AUTO_INCREMENT pour la table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id_prod` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_prod` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
