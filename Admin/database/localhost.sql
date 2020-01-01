f39b21279a06ed82385b241e3ab49ef50ba1c14d
tahiriabdo131 <56969009+tahiriabdo131@users.noreply.github.com>
2020-01-01 14:37:29 -0800

    i add areal data to DB
    doing by tahiri

--- Admin/database/localhost.sql
+++ Admin/database/localhost.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.3
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost
--- Généré le :  mar. 31 déc. 2019 à 15:14
--- Version du serveur :  10.2.27-MariaDB-cll-lve
--- Version de PHP :  5.5.38
+-- Hôte : 127.0.0.1
+-- Généré le :  jeu. 02 jan. 2020 à 08:36
+-- Version du serveur :  10.4.10-MariaDB
+-- Version de PHP :  7.1.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,8 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `mundiaitca_db`
 --
-CREATE DATABASE IF NOT EXISTS `mundiaitca_db` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `mundiaitca_db`;
 
 -- --------------------------------------------------------
 
@@ -98,10 +96,14 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id_cat`, `label_cat`, `desc_cat`, `status`) VALUES
-(1, 'Category1', 'Description of category1 google lictionnaire', 0),
-(2, 'category', 'description hhh', 1),
-(3, 'Category', 'descr', 0),
-(4, 'Pc', 'hzefzefjezf', 0);
+(1, 'cat 1', 'Description of category1 google lictionnaire', 1),
+(2, 'cat2', 'description hhh', 1),
+(3, 'cat3', 'descr', 1),
+(4, 'cat4', 'desc 4', 1),
+(5, 'cat 5', 'desc 5', 1),
+(6, 'cat6', 'desc 6', 1),
+(7, 'cat7', 'desc 7', 1),
+(8, 'cat8', 'desc 8', 1);
 
 -- --------------------------------------------------------
 
@@ -141,9 +143,10 @@ CREATE TABLE `order` (
 --
 
 CREATE TABLE `order_product` (
-  `id_order` int(20) NOT NULL,
-  `id_cost` int(20) NOT NULL,
-  `quantity` int(10) NOT NULL
+  `quantity` int(10) NOT NULL,
+  `id_prod` int(11) NOT NULL,
+  `id_order` int(11) NOT NULL,
+  `id_order_product` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -161,7 +164,7 @@ CREATE TABLE `product` (
   `desc_prod` varchar(100) NOT NULL,
   `image_prod` varchar(100) NOT NULL,
   `active` tinyint(1) DEFAULT 0,
-  `quantity_prod` decimal(11,2) NOT NULL
+  `quantity_prod` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -169,13 +172,20 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id_prod`, `label_prod`, `id_cat`, `price_prod`, `weight_prod`, `desc_prod`, `image_prod`, `active`, `quantity_prod`) VALUES
-(1, 'Whirepool Refrigerator 1.20X0.70 9.99', 0, '3000.00', '20.90', 'bllllllllaaaaa description', 'img is here', 0, '10.00'),
-(5, 'pc i5 ', 0, '2000.00', '23.30', 'tdesription 2', 'balanla', 1, '2.00'),
-(6, '', 0, '0.00', '0.00', '', '', 0, '0.00'),
-(7, 'Asus', 0, '5000.00', '6.00', 'PC Portable', 'Koala.jpg', 0, '5.00'),
-(8, 'PC HG', 0, '6666.00', '999.00', 'ffdgdgd', 'Penguins.jpg', 0, '4464.00'),
-(9, 'P1', 0, '2.00', '2.00', 'DESC', 'forest-3840x2160-4k-5k-wallpaper-trees-sunlight-fog-autumn-5726.jpg', 0, '2.00'),
-(10, 'P', 3, '2.00', '2.00', 'DESC', 'forest-3840x2160-4k-5k-wallpaper-trees-sunlight-fog-autumn-5726.jpg', 0, '2.00');
+(1, 'Whirepool Refrigerator 1.20X0.70 9.99', 0, '8000.00', '20.90', 'bllllllllaaaaa description', 'img is here', 0, 10),
+(5, 'Huawei Mediapad Téléphone Tablette', 2, '8000.00', '23.30', 'tdesription 2', 'images/huwawei.jpg', 1, 2),
+(6, 'Casque Bluetooth B&O H7 Sans Fil', 2, '1000.00', '0.00', '', 'images/casque.png', 1, 1),
+(7, 'sony playstation 4 Pro 1 To', 0, '5000.00', '6.00', 'PC Portable', 'images/sony.jpg', 1, 5),
+(8, 'The Apple Watch Series 5 ', 2, '500.00', '999.00', 'ffdgdgd', 'images/apple.jpg  ', 1, 4464),
+(9, 'AeroCool Cylon RGB Mid Tower with Acrylic Side Window, Black', 4, '36999.99', '2.00', 'DESC', 'images/id8.jpg', 1, 2),
+(10, 'P', 3, '2.00', '2.00', 'DESC', 'forest-3840x2160-4k-5k-wallpaper-trees-sunlight-fog-autumn-5726.jpg', 0, 2),
+(11, 'Ifecco Bluetooth Headphones, 4 in 1 Upgrade Bluetooth Over-Ear Headsets', 4, '799.99', '23.80', 'description ', 'images/id9.jpg', 1, 4),
+(12, 'Ifecco Bluetooth Headphones, 4 in 1 Upgrade Bluetooth Over-Ear Headsets', 5, '799.99', '23.80', 'description ', 'images/id9.jpg', 1, 4),
+(13, 'BLU Studio Mini -5.5HD Smartphone, 32GB+2GB Ram -Black', 3, '9999.99', '23.45', 'decccccc', 'images/phone9.png', 1, 23),
+(14, 'MSI P65 Creator-1084 15.6\" 4K UHD Display, Ultra-Thin and Light', 6, '3999.99', '233.89', 'deccccc', 'images/laptop.jpg', 1, 23),
+(15, 'MSI P65 Creator-1084 15.6\" 4K, Ultra-Thin and Light,RTX Studio Laptop', 2, '14999.99', '244.90', 'decc', 'images/laptop.jpg', 1, 11),
+(16, 'abdelali', 4, '10000.00', '200.20', 'desc   vvv', 'images/laptop.jpg', 1, 3),
+(17, 'tahiri', 4, '12000.00', '23.90', 'gggggg', '', 1, 2);
 
 --
 -- Index pour les tables déchargées
@@ -221,7 +231,7 @@ ALTER TABLE `order`
 -- Index pour la table `order_product`
 --
 ALTER TABLE `order_product`
-  ADD PRIMARY KEY (`id_order`,`id_cost`);
+  ADD PRIMARY KEY (`id_order_product`);
 
 --
 -- Index pour la table `product`
@@ -249,7 +259,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT pour la table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id_cat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_cat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT pour la table `costumer`
@@ -264,10 +274,16 @@ ALTER TABLE `order`
   MODIFY `id_order` int(20) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT pour la table `order_product`
+--
+ALTER TABLE `order_product`
+  MODIFY `id_order_product` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT pour la table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id_prod` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_prod` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
