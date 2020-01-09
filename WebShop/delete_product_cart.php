e9d203b02d5411d12aa5b1c910690c3dfbc3b706
tahiriabdo131 <56969009+tahiriabdo131@users.noreply.github.com>
2020-01-08 17:21:23 -0800

    doing by tahiri
    delete product from cart done

--- WebShop/delete_product_cart.php
+++ WebShop/delete_product_cart.php
@@ -1,1 +1,14 @@
-<?php echo "you have to delete this product from DB "?>
+<?php
+    include_once("connection/db_connection.php");
+    $id_product = $_GET["id_product"];
+	// sql to delete a record
+	$sql = "DELETE FROM cart WHERE id_product=$id_product";
+	if ($conn->query($sql) === TRUE) {
+	echo '<script type="text/javascript">'; 
+	echo 'alert("Deleted successfully !!");'; 
+	echo 'window.location.href = "cart.php"';
+	echo '</script>';
+	} else {
+	    echo "Error deleting record: " . $conn->error;
+	}
+?>
