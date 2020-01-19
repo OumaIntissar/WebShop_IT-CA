6bd75032c5241fa0c9065bb9ea445f63318e70f3
OumaIntissar <oumaintissar@gmail.com>
2020-01-19 22:49:05 +0100

    prod detail

--- WebShop/product.php
+++ WebShop/product.php
@@ -1,6 +1,8 @@
 <!-- include the header-->
 <?php include_once "header.php";?>
-
+<style type="text/css">
+		.product_price{color:#FFD700!important;font-size:25px;}
+	</style>
 
 <!-- Single Product -->
 <div class="single_product">
@@ -30,46 +32,31 @@
 					<!-- Description -->
 					<div class="col-lg-5 order-3">
 						<div class="product_description">
-					<div class="product_category">'.$row_cat["label_cat"].'</div>
-					<div class="product_name">'.$row_prod["label_prod"].'</div>
-					<div class="product_text"><p>'.$row_prod["desc_prod"].'</p></div>
-					<div class="order_info d-flex flex-row">
-							<div class="clearfix" style="z-index: 1000;">
-
-								<!-- Product Quantity -->
-								<div class="product_quantity clearfix">
-									<span>Quantity: </span>
-									<input id="quantity_input" type="number" pattern="[0-9]*" value="1">
-									<div class="quantity_buttons">
-										<div id="quantity_inc_button" class="quantity_inc quantity_control"><i class="fas fa-chevron-up"></i></div>
-										<div id="quantity_dec_button" class="quantity_dec quantity_control"><i class="fas fa-chevron-down"></i></div>
-									</div>
-								</div>
-
-							</div>
-
-							<div class="product_price">
+							<div class="product_category">'.$row_cat["label_cat"].'</div>
+							<div class="product_name">'.$row_prod["label_prod"].'</div>
+							<div class="product_text"><p>'.$row_prod["desc_prod"].'</p></div>
+							<div >
 								
-								'.$row_prod["price_prod"].' MAD</div> <br><br>
-							<div class="button_container">' ?>
-								<!-- Tahiri Start -->
-								<form method="GET" action="check_add_to_cart_if_connecter.php">
-									<input type="hidden" name="id_product" value="<?php echo $id_prod ?>">
-									<input type="hidden" name="number_product_in_cart" value="" >										
-		                            <button type="submit" name="btn_add_to_cart" value="click" style="border: none;color:white; border-radius: 40px!important;margin-top: 200px;margin-left: -230px;">
-			                            <a class="btn btn-info btn-lg"  >
-			                                <span class="glyphicon glyphicon-shopping-cart"></span>  Add To Cart  
-			                            </a>
-			                        </button>
-		                        </form>	
-		                        <!-- Tahiri End -->
-							<?php echo 
-							'</div>
-					</div>';
-				}
-					?>							
-			</div>
-		</div>
+								<div class="product_price" style="margin-top: 30px;">'.$row_prod["price_prod"].' MAD</div> 
+								<div class="button_container" style="margin-top: 0px;">' ?>
+									<!-- Tahiri Start -->
+									<form method="GET" action="check_add_to_cart_if_connecter.php">
+										<input type="hidden" name="id_product" value="<?php echo $id_prod ?>">
+										<input type="hidden" name="number_product_in_cart" value="" >										
+										<button type="submit" name="btn_add_to_cart" value="click" style="border: none;color:white; border-radius: 40px!important;margin-top: 30px; margin-bottom: 40px;">
+											<a class="btn btn-info btn-lg"  >
+												<span class="glyphicon glyphicon-shopping-cart"></span>  Add To Cart  
+											</a>
+										</button>
+									</form>	
+									<!-- Tahiri End -->
+								<?php echo 
+								'</div>
+							</div>';
+							}
+							?>							
+						</div>
+					</div>
 	</div>
 </div>
 <br>
--- WebShop/sign_in.php
+++ WebShop/sign_in.php
@@ -3,7 +3,7 @@
     include_once("connection/db_connection.php");
     if(isset($_GET["btn_sing_in"])){
 	    $username = $_GET['username'];
-	    $password = md5($_GET['password']);
+	    $password = $_GET['password'];
 
 	    $sql_costumers = "SELECT * FROM costumer";
 	    $result_costumers = $conn->query($sql_costumers);
